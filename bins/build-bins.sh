0cff287e707872a85c5283f0fa9a2eb86bc0d025
David Rios <david.rios.gomes@gmail.com>
2026-01-25 18:02:37 -0300

    Add services

--- bins/build-bins.sh
+++ bins/build-bins.sh
@@ -5,8 +5,12 @@ docker run --rm -it -v $PWD:/code minirouter-builder sh -c '
 set -e
 DEST=/opt/minirouter
 cd /code
+mkdir -p $DEST/etc/default $DEST/etc/systemd/system
+cp *.default $DEST/etc/default
+cp *.service $DEST/etc/systemd/system/
 make clean
 make install DEST=$DEST -j4
+
 cat <<'EOF' > $DEST/bin/install-deps.sh
 #!/bin/bash
 if test \$(id -u) != 0; then
@@ -16,6 +20,22 @@ fi
 apt-get update && apt-get install -y libczmq4 libfreetype6 ttf-bitstream-vera
 EOF
 chmod +x $DEST/bin/install-deps.sh
+
+cat <<'EOF' > $DEST/bin/setup-services.sh
+#!/bin/bash
+if test \$(id -u) != 0; then
+	echo This program needs to be run as root
+	exit 1
+fi
+test -f /etc/default/gpio_buttons_server || cp $DEST/etc/default/gpio_buttons_server.default /etc/default/gpio_buttons_server
+test -f /etc/default/ssd1306_i2c_server || cp $DEST/etc/default/ssd1306_i2c_server.default /etc/default/ssd1306_i2c_server
+cp $DEST/etc/systemd/system/* /etc/systemd/system/
+systemctl daemon-reload
+systemctl enable --now gpio_buttons_server
+systemctl enable --now ssd1306_i2c_server
+EOF
+chmod +x $DEST/bin/setup-services.sh
+
 cd /
 tar cfvz /code/minirouter.tar.gz opt/minirouter
 cd /code && make clean
